9e7d54813a4b5ecf2b6fdc88ad4e659a70dae336
robin-UI <robinme1971@gmail.com>
2025-01-14 19:25:40 +0530

    ignite the eract app

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client"
+
 const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child" }, [
     React.createElement("h1", {}, "This is h1 child"),
-    React.createElement("h2", {}, "This is h2 child"),
+    React.createElement("h2", {}, "This is Akshey sine"),
   ]),
   React.createElement("div", { id: "child2" }, [
     React.createElement("h1", {}, "This is h1 child2"),
@@ -19,7 +22,6 @@ const heading = React.createElement(
   "Hello world from React"
 );
 
-console.log(heading);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(parent);
